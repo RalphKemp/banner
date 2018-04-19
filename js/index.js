fa51179bbd9bf29b9e747167533a7c529d9d4231
ralph kemp <ralphk3mp@gmail.com>
2018-04-19 03:34:46 +0100

    btn

--- js/index.js
+++ js/index.js
@@ -30,6 +30,7 @@ t1.to(".text-two", 1, {opacity: 0, delay: 1.6});
 
 //slide 3
 t1.from(".stocks-div", 0.5, {opacity: 0});
+t1.from(".stocks-div-square", 0.5, {opacity: 0});
 t1.from(".text-three", 0.8, {opacity: 0});
 t1.from(".slogan", 0.8, {opacity: 0});
 t1.from(".btn-medium", 0.8, {opacity: 0, delay: 0.3});
